cde8da3f24fd70395148bb4b51e12b5fb0535dce
Varun Ramesh <varun@goguava.ai>
2026-07-27 15:37:00 -0700

    Update devc formula to 0.5.0

--- Formula/devc.rb
+++ Formula/devc.rb
@@ -1,21 +1,36 @@
 class Devc < Formula
-  desc "Simpler alternative to the Dev Containers CLI"
+  desc "A simpler alternative to the Dev Containers CLI"
   homepage "https://github.com/rameshvarun/devc"
-  version "0.4.0"
+  version "0.5.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/rameshvarun/devc/releases/download/v0.4.0/devc-0.4.0-darwin-x86_64"
-      sha256 "8334ed80c7c164551305d3d657d7b6a53c44ef313e993e3edac0b7317b2ea6eb"
+      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-darwin-x86_64"
+      sha256 "818dc96f5297943e6f98cd1aa99ba2449077258f452c49b63f57ac432d9ac5ab"
     end
     on_arm do
-      url "https://github.com/rameshvarun/devc/releases/download/v0.4.0/devc-0.4.0-darwin-arm64"
-      sha256 "00cda53bd586861550690b79f62546c3d67c2294482e908b82b35581af28c79c"
+      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-darwin-arm64"
+      sha256 "851eb515d85311902a4ad976daf8e4dc29efc40a9d3193a96fc6d47219fdea78"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-linux-x86_64"
+      sha256 "e89992972d10ec39ab016241ddf08e2ffde5d6bba58e6f23af7e862030a66cc0"
+    end
+    on_arm do
+      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-linux-aarch64"
+      sha256 "0f8a87426303ea936f878713f6f98ce0228b5dacfee2371644a77d7ff0d7024b"
     end
   end
 
   def install
-    binary = Hardware::CPU.arm? ? "devc-0.4.0-darwin-arm64" : "devc-0.4.0-darwin-x86_64"
+    binary = if OS.mac?
+      Hardware::CPU.arm? ? "devc-0.5.0-darwin-arm64" : "devc-0.5.0-darwin-x86_64"
+    else
+      Hardware::CPU.arm? ? "devc-0.5.0-linux-aarch64" : "devc-0.5.0-linux-x86_64"
+    end
     bin.install binary => "devc"
   end
 
