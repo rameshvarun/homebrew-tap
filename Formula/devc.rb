f8e57e699761b101dea7d80550aa4af35c724acf
Varun Ramesh <varun@goguava.ai>
2026-07-30 18:18:41 -0700

    Update devc formula to 0.6.0

--- Formula/devc.rb
+++ Formula/devc.rb
@@ -1,35 +1,35 @@
 class Devc < Formula
   desc "A simpler alternative to the Dev Containers CLI"
   homepage "https://github.com/rameshvarun/devc"
-  version "0.5.0"
+  version "0.6.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-darwin-x86_64"
-      sha256 "818dc96f5297943e6f98cd1aa99ba2449077258f452c49b63f57ac432d9ac5ab"
+      url "https://github.com/rameshvarun/devc/releases/download/v0.6.0/devc-0.6.0-darwin-x86_64"
+      sha256 "2ae1a7abc60550a39ffda89bae893b10e0b8236ed7437275e5e380ad8da51ee0"
     end
     on_arm do
-      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-darwin-arm64"
-      sha256 "851eb515d85311902a4ad976daf8e4dc29efc40a9d3193a96fc6d47219fdea78"
+      url "https://github.com/rameshvarun/devc/releases/download/v0.6.0/devc-0.6.0-darwin-arm64"
+      sha256 "8f2f9fe72784e4c4188e9f980a47fe7a0df61b05a4a831c2c05c352431a9f039"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-linux-x86_64"
-      sha256 "e89992972d10ec39ab016241ddf08e2ffde5d6bba58e6f23af7e862030a66cc0"
+      url "https://github.com/rameshvarun/devc/releases/download/v0.6.0/devc-0.6.0-linux-x86_64"
+      sha256 "14a18dd4270b63bfc4eb4870c1385ae20d0155ef0b0235421513095171c8169a"
     end
     on_arm do
-      url "https://github.com/rameshvarun/devc/releases/download/v0.5.0/devc-0.5.0-linux-aarch64"
-      sha256 "0f8a87426303ea936f878713f6f98ce0228b5dacfee2371644a77d7ff0d7024b"
+      url "https://github.com/rameshvarun/devc/releases/download/v0.6.0/devc-0.6.0-linux-aarch64"
+      sha256 "b1d2d7c45922789b0ad2a1ea59a3239ad9a9a67489c4cd080463f1550656f145"
     end
   end
 
   def install
     binary = if OS.mac?
-      Hardware::CPU.arm? ? "devc-0.5.0-darwin-arm64" : "devc-0.5.0-darwin-x86_64"
+      Hardware::CPU.arm? ? "devc-0.6.0-darwin-arm64" : "devc-0.6.0-darwin-x86_64"
     else
-      Hardware::CPU.arm? ? "devc-0.5.0-linux-aarch64" : "devc-0.5.0-linux-x86_64"
+      Hardware::CPU.arm? ? "devc-0.6.0-linux-aarch64" : "devc-0.6.0-linux-x86_64"
     end
     bin.install binary => "devc"
   end
